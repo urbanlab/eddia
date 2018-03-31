bd03e9faf7964384d683dffb4a912133c68d4f87
Vinchenzo Guibert <vinchenzo.guibert@epitech.eu>
2018-03-31 14:58:31 +0200

    remove old server folder

--- server/public/js/init.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-	// Disable scrolling
-	document.body.addEventListener('touchmove', function(event) {
-		event.preventDefault();
-	});
-};
--- server/public/js/sockets.js
+++
@@ -1,8 +0,0 @@
-
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:3000');
-
-console.log("socket connected", socket.connected);
-socket.on('get manager', function(data) {
-	console.log(data.msg);
-})
