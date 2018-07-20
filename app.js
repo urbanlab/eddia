b8e41c1dfe45df72e7db6df829af7305d31a3e7e
NatanBT <natan.baron-trocellier@ecl15.ec-lyon.fr>
2018-07-20 16:53:38 +0200

    Le topic par défaut est maintenant DEMO. Ajout du système de versions du corpus.

--- app.js
+++ app.js
@@ -58,7 +58,6 @@ app.get('/:topic/:id', function(req, res, next) {
 	};
 });
 
-
 //--------------------------------------------------------------------------------
 // Clear cache and initialize path variables
 //--------------------------------------------------------------------------------
@@ -67,7 +66,7 @@ var structs_path = "./datas/struct/";         // Path of the cache associated wi
 var topics_path = "./datas/topics/";		  // Path of the databases associated with each topic of discussion
 
 var topics = get_dir_content(topics_path);
-var default_topic = topics[0];
+var default_topic = 'DEMO';
 
 clear_folder(structs_path);                   // Clear all caches
 var id_index = 1;                             // When a new room is created, its ID is created using this variable
@@ -98,7 +97,7 @@ function clear_folder(dir_path) {
 function update_id_index(dir_path) {
 	// When a room is created, generate a unique ID for it 
 	var files = get_dir_content(dir_path);
-	var file_numbers = files.map((filename) => (parseInt(filename.split('.')[0])));
+	var file_numbers = files.map((file_name) => (parseInt(file_name.split('.')[0])));
 	if(file_numbers == []) {
 		return 1
 	} else {
@@ -115,6 +114,62 @@ function isEmpty(obj) {
     return true;
 }
 
+function get_data(filename) {
+	// Get the data contained in the file of path filename
+	try {
+    		return JSON.parse(fs.readFileSync(filename, "utf8"));
+	} catch (err) {
+		return null;
+	}
+}
+
+function write_data(filename, datas) {
+	// Create the file of path filename and put datas in it.
+	// If it already exists, replace its content with datas.
+	fs.writeFileSync(filename, JSON.stringify(datas));
+}
+
+function get_last_save(dir_path) {
+	// Returns the path to the last version of the corpus of path dir_path
+	var files = get_dir_content(dir_path);
+	var saves = files.filter((file_name) => (file_name.endsWith('.save.json')));
+	if(saves.length == 0) {
+		console.log(dir_path + 'words.json');
+		return dir_path + 'words.json';
+	} else {
+		console.log('')
+		var lastSave = saves.reduce(function(acc, file_name) {
+			var date_accumulator = new Date(acc.split('__')[1].split('.')[0]);
+			var date_file = new Date(file_name.split('__')[1].split('.')[0]);
+			var lastest_date = (date_file > date_accumulator) ? date_file : date_accumulator;
+			lastest_date = 'words__' + lastest_date.toString().split(' GMT')[0] + '.save.json';
+			return lastest_date;
+		});
+		lastSave = lastSave.toString().split(' GMT')[0];
+		console.log(lastSave);
+		return dir_path + lastSave;
+	}
+};
+
+function create_new_save(dir_path, datas) {
+	// Create a new save of the corpus of path 'dir_path' and of content 'datas',
+	// and name it words__currentDate.save.json
+	var date = new Date().toString().split(' GMT')[0];
+	var save_file_name = dir_path + 'words__' + date + '.save.json';
+	write_data(save_file_name, datas);
+};
+
+function make_human_reader_friendly(words_file_path) {
+	// Transform a word corpus to make it more human-readable and editable
+	var fileString = fs.readFileSync(words_file_path, "utf8");
+	fileString = fileString.replace('{', '{\n    ');
+	fileString = fileString.replace('}', '\n}');
+	fileString = fileString.replace(/:\[/g, ': [\n      ');
+	fileString = fileString.replace(/"],/g , '"\n    ],\n    ');
+	fileString = fileString.replace(/",/g , '",\n      ');
+	fs.writeFileSync(words_file_path, fileString);
+};
+
 //--------------------------------------------------------------------------------
 // Communication with the frontend
 //--------------------------------------------------------------------------------
@@ -125,6 +180,7 @@ io.on('connection', function(socket) {
 	var filename;							// path to the cache of the current room (contains the data of the dialogue)
 	var model_interest_words;               // objects that contains the words Eddia is listening for, classified by interest
 	var model_contents;						// object that contains the contents associated with each (interest, word) couple
+	var database_modified = false;          // boolean set to true if words were added to the word corpus using voice commands
 
 	socket.on('room', function(client) {
 		// When a room is created, it sends data to the server. Use this data to initialize the backend and finalize the creation
@@ -144,9 +200,9 @@ io.on('connection', function(socket) {
 		valid_id_indexes.push(id_index);
 		id_index = update_id_index(structs_path);
 
-		model_interest_words = require(topic_path + 'words.json');
+		model_interest_words = require(get_last_save(topic_path));
 		model_contents = require(topic_path + 'contents.json');
-		socket.join(String(room_id));
+		socket.join(room_id);
 		socket.emit('topics', {'topic_list':topics, 'current_topic':room_topic});
 
 		//--------------------------Communication with the Frontend------------------------
@@ -165,7 +221,8 @@ io.on('connection', function(socket) {
 
 		socket.on('voice/add_new_word', function(transcription) {
 			// When an instruction is sent to add a new word, process it and add the new
-			// (interest, word) couple to the room's cache
+			// (interest, word) couple to the room's cache. If it is not in the word corpus,
+			// add it to the later.
 			var transcription_array = transcription.split(' ');
 			transcription_array = transcription_array.filter((x) => (x != ' '));
 			transcription_array.map((x) => x.toLowerCase());
@@ -177,10 +234,17 @@ io.on('connection', function(socket) {
 				(index_interest < index_word - 1))
 				{
 
-				var interest = transcription_array.slice(index_interest + 1, index_word).join(' '),
-					word = transcription_array.slice(index_word + 1).join(' ');
+				var interest = transcription_array.slice(index_interest + 1, index_word).join(' ').trim(),
+					word = transcription_array.slice(index_word + 1).join(' ').trim();
 
 				add(word, interest);
+				if(model_interest_words[interest] == undefined) {
+					model_interest_words[interest] = [word];
+					database_modified = true;
+				} else if(!model_interest_words[interest].includes(word)) {
+					model_interest_words[interest].push(word);
+					database_modified = true;
+				};
 			}
 		});
 
@@ -188,36 +252,35 @@ io.on('connection', function(socket) {
 			// When the transcription of a dialogue chunk is sent to the the server, search for
 			// interest words into it. If an interest word is found, add it to the cache and order
 			// the client to create the correspoonding bubbles
+			console.log(transcription);
 			for (interest in model_interest_words) {
 				for(const word of model_interest_words[interest]) {
-					if(transcription.indexOf(word) != -1) {
+					if(transcription.includes(word)) {
 						add(word, interest);
 					}
 				}
 			}
 		});
+
+		socket.on('disconnect', function() {
+			// When a page of the application is closed, and if the word corpus was modified, then
+			// create save of the new version of the corpus
+			console.log('Room ' + room_topic + '/' + room_id + ' disconnected.');
+			if(database_modified) {
+				create_new_save(topic_path, model_interest_words);
+				make_human_reader_friendly(get_last_save(topic_path));
+				console.log('New corpus version created in file' + get_last_save(topic_path));
+			} else {
+				console.log('No word was added to the corpus.');
+			}
+		});
+
 	});
         
     //-------------------Calculation functions used by the backend-----------------------
     // Notes: - |adding a word contained into several interests => add all interests
     //        - |removing                                       => remove the specified interest only
     //        - a word can appear in several interests, but a content is bound to only one (interest, word) couple
-
-    function get_data(filename) {
-    	// Get the data contained in the file of path filename
-		try {
-        		return JSON.parse(fs.readFileSync(filename, "utf8"));
-		} catch (err) {
-			return null;
-		}
-    }
-    
-    function write_data(filename, datas) {
-    	// Create the file of path filename and put datas in it.
-    	// If it already exists, replace its content with datas.
-    	fs.writeFileSync(filename, JSON.stringify(datas));
-    }
-
 
 	function search_in_model_contents(interest, word) {
 		// Returns the contents associated with an (interest, word) couple
@@ -305,7 +368,6 @@ io.on('connection', function(socket) {
 			console.log('removed word:', word)
 			delete datas[room_topic][interest][word];
 			if (isEmpty(datas[room_topic][interest])) {
-				console.log('coucou');
 				console.log('removed interest:', interest);
 				delete datas[room_topic][interest]
 			}
